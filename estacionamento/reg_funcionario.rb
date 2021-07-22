8456610e7dbfa30d215ec4233a02b4346f521ada
Leandro Santos <q4ck@hotmail.com>
2021-07-22 14:29:25 -0300

    Update reg_funcionario.rb

--- estacionamento/reg_funcionario.rb
+++ estacionamento/reg_funcionario.rb
@@ -1,16 +1,8 @@
-#require_relative 'index'
-
 class funcionario
     attr_accessor :nome, :idade :cargo
 
     def initialize (nome, idade, cargo)
-    if (opcao == 2)
-        print 'Nome: '
-        @nome = gets.to_s
-        print 'Idade: '
-        @idade = gets.to_s
-        print 'Cargo: '
-        @cargo = gets.to_s
+    
     end
 
 
